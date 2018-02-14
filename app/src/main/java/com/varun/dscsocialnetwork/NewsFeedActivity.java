9e0013ae373ea7b6025de12aaf069c7c1aa82446
Varun Ranganathan <vvvrock@gmail.com>
2018-02-14 11:28:35 +0530

    Updated Firebase!

--- app/src/main/java/com/varun/dscsocialnetwork/NewsFeedActivity.java
+++ app/src/main/java/com/varun/dscsocialnetwork/NewsFeedActivity.java
@@ -8,6 +8,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,14 +36,46 @@ public class NewsFeedActivity extends AppCompatActivity {
                 (getApplicationContext(), android.R.layout.simple_list_item_1,
                         android.R.id.text1, dataSource);
         l1.setAdapter(myAdapter);
+        final DatabaseReference d1  =
+                FirebaseDatabase.getInstance().
+                        getReference("posts");
+        d1.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Post p1 = dataSnapshot.getValue(Post.class);
+                String messageToDisplay = p1.getUserName()+": "+p1.getPostText();
+                dataSource.add(messageToDisplay);
+                myAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String message = e1.getEditableText().toString();
-                message = userName + ": "+message;
-                dataSource.add(message);
-                myAdapter.notifyDataSetChanged();
+                String pid = d1.push().getKey();
+                Post p1 = new Post(userName, message, pid);
+                d1.child(pid).setValue(p1);
                 e1.setText("");
             }
         });
